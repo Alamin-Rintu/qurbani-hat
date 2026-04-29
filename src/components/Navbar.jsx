833c4e0314ce80281734a94325a62e25553482fd
Alamin-Rintu <alaminrintu01@gmail.com>
2026-04-29 21:49:45 +0600

    setup assignment and add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="border-b border-gray-300 px-2">
+      <nav className=" flex justify-between items-center  py-3 max-w-7xl mx-auto w-full">
+        <div className="flex gap-2 items-center">
+          <Image
+            src={"/quarbani.jpg"}
+            alt="logo"
+            loading="eager"
+            width={60}
+            height={50}
+            className="object-cover h-auto w-auto"
+          />
+          <h3 className="font-black text-lg">QurbaniHat</h3>
+        </div>
+
+        <ul className="flex items-center gap-5 text-sm">
+          <li>
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link href={"/animals"}>All Animals</Link>
+          </li>
+        </ul>
+
+        <div className="flex gap-4">
+          <ul className="flex items-center gap-3 text-sm">
+            <li>
+              <Link href={"/signup"}>SignUp</Link>
+            </li>
+            <li>
+              <Link href={"/signin"}>SignIn</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
