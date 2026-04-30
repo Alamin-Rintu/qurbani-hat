287498bfee80ff91ee5584079120158581ea055d
Alamin-Rintu <alaminrintu01@gmail.com>
2026-04-30 12:08:18 +0600

    added login and register auth

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,11 +1,109 @@
-import React from 'react';
+"use client";
+import { authClient } from "@/lib/auth-client";
+import { Check, Eye, EyeSlash } from "@gravity-ui/icons";
+import {
+  Button,
+  Description,
+  FieldError,
+  Form,
+  Input,
+  InputGroup,
+  Label,
+  TextField,
+} from "@heroui/react";
+import Link from "next/link";
+import { useState } from "react";
 
 const LoginPage = () => {
-    return (
-        <div>
-            
+  const [isVisible, setIsVisible] = useState(false);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log({ email, password });
+
+    const { data, error } = await authClient.signIn.email({
+      email,
+      password,
+      rememberMe: true,
+      callbackURL: "/",
+    });
+    console.log(data, error);
+  };
+  return (
+    <div className="flex justify-center mt-5">
+      <Form
+        className="flex w-96 flex-col gap-4 border border-gray-300 p-5 rounded-xl"
+        onSubmit={onSubmit}
+      >
+        <h3 className="text-center text-2xl font-bold">LogIn Now</h3>
+
+        {/* email */}
+
+        <TextField
+          isRequired
+          type="email"
+          validate={(value) => {
+            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+              return "Please enter a valid email address";
+            }
+            return null;
+          }}
+        >
+          <Label>Email</Label>
+          <Input name="email" placeholder="Your Email" />
+          <FieldError />
+        </TextField>
+
+        {/* password */}
+        <TextField className="w-full " name="password">
+          <Label>Password</Label>
+          <InputGroup>
+            <InputGroup.Input
+              className="w-full max-w-[280px]"
+              type={isVisible ? "text" : "password"}
+            />
+            <InputGroup.Suffix className="pr-0">
+              <Button
+                isIconOnly
+                aria-label={isVisible ? "Hide password" : "Show password"}
+                size="sm"
+                variant="ghost"
+                onPress={() => setIsVisible(!isVisible)}
+              >
+                {isVisible ? (
+                  <Eye className="size-4" />
+                ) : (
+                  <EyeSlash className="size-4" />
+                )}
+              </Button>
+            </InputGroup.Suffix>
+          </InputGroup>
+        </TextField>
+
+        <div className="flex gap-2">
+          <Button type="submit">
+            <Check />
+            Log in
+          </Button>
+          <Button type="reset" variant="secondary">
+            Reset
+          </Button>
         </div>
-    );
+        {/* <p className="font-semibold">
+          Already have an account?
+          <Link href={"/login"}>
+            <Button
+              className={"text-purple-500 font-medium text-lg bg-transparent"}
+            >
+              Log in
+            </Button>
+          </Link>
+        </p> */}
+      </Form>
+    </div>
+  );
 };
 
 export default LoginPage;
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,25 +1,72 @@
 "use client";
-import { Check } from "@gravity-ui/icons";
+import { authClient } from "@/lib/auth-client";
+import { Check, Eye, EyeSlash } from "@gravity-ui/icons";
 import {
   Button,
   Description,
   FieldError,
   Form,
   Input,
+  InputGroup,
   Label,
   TextField,
 } from "@heroui/react";
+import Link from "next/link";
+import { useState } from "react";
 
 const RegisterPage = () => {
-  const onSubmit = (e) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const onSubmit = async (e) => {
     e.preventDefault();
+    const name = e.target.name.value;
+    const image = e.target.image.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log({ name, image, email, password });
+
+    const { data, error } = await authClient.signUp.email({
+      name,
+      email,
+      password,
+      image,
+      callbackURL: "/",
+    });
+    console.log(data, error);
   };
+
   return (
     <div className="flex justify-center mt-5">
-      <Form className="flex w-96 flex-col gap-4 border border-gray-300 p-5 rounded-xl" onSubmit={onSubmit}>
+      <Form
+        className="flex w-96 flex-col gap-4 border border-gray-300 p-5 rounded-xl"
+        onSubmit={onSubmit}
+      >
+        <h3 className="text-center text-2xl font-bold">Register Now</h3>
+        {/* name */}
         <TextField
           isRequired
-          name="email"
+          validate={(value) => {
+            if (value.length < 3) {
+              return "Name must be at least 3 characters";
+            }
+            return null;
+          }}
+        >
+          <Label>Name</Label>
+          <Input name="name" placeholder="Your Name" />
+          <FieldError />
+        </TextField>
+
+        {/* image URL */}
+        <TextField isRequired type="text">
+          <Label>Image URL</Label>
+          <Input name="image" placeholder="Image URL" />
+          <FieldError />
+        </TextField>
+
+        {/* email */}
+
+        <TextField
+          isRequired
           type="email"
           validate={(value) => {
             if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
@@ -29,43 +76,55 @@ const RegisterPage = () => {
           }}
         >
           <Label>Email</Label>
-          <Input placeholder="john@example.com" />
+          <Input name="email" placeholder="Your Email" />
           <FieldError />
         </TextField>
-        <TextField
-          isRequired
-          minLength={8}
-          name="password"
-          type="password"
-          validate={(value) => {
-            if (value.length < 8) {
-              return "Password must be at least 8 characters";
-            }
-            if (!/[A-Z]/.test(value)) {
-              return "Password must contain at least one uppercase letter";
-            }
-            if (!/[0-9]/.test(value)) {
-              return "Password must contain at least one number";
-            }
-            return null;
-          }}
-        >
+
+        {/* password */}
+        <TextField className="w-full " name="password">
           <Label>Password</Label>
-          <Input placeholder="Enter your password" />
-          <Description>
-            Must be at least 8 characters with 1 uppercase and 1 number
-          </Description>
-          <FieldError />
+          <InputGroup>
+            <InputGroup.Input
+              className="w-full max-w-[280px]"
+              type={isVisible ? "text" : "password"}
+            />
+            <InputGroup.Suffix className="pr-0">
+              <Button
+                isIconOnly
+                aria-label={isVisible ? "Hide password" : "Show password"}
+                size="sm"
+                variant="ghost"
+                onPress={() => setIsVisible(!isVisible)}
+              >
+                {isVisible ? (
+                  <Eye className="size-4" />
+                ) : (
+                  <EyeSlash className="size-4" />
+                )}
+              </Button>
+            </InputGroup.Suffix>
+          </InputGroup>
         </TextField>
+
         <div className="flex gap-2">
           <Button type="submit">
             <Check />
-            Submit
+            Register Now
           </Button>
           <Button type="reset" variant="secondary">
             Reset
           </Button>
         </div>
+        <p className="font-semibold">
+          Already have an account?
+          <Link href={"/login"}>
+            <Button
+              className={"text-purple-500 font-medium text-lg bg-transparent"}
+            >
+              Log in
+            </Button>
+          </Link>
+        </p>
       </Form>
     </div>
   );
